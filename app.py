80fedcfa8592638e3104eefdd402de1be2db7f4f
Igor Pankov <igor9mail@gmail.com>
2019-08-25 22:08:30 +0300

    Added Chalice app

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
+from chalice import Chalice
+
+app = Chalice(app_name='aws-service')
 
 
-from utils.utils.strings import compare_strings
+@app.route('/')
+def index():
+    return {'hello': 'world AWS Lambda'}
 
 
-def start_app():
-    print('Starting application...')
-    print(compare_strings('A', 'B'))
-    print('Done')
-
-
-if __name__ == '__main__':
-    start_app()
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
---
+++ cli_test_app.py
@@ -0,0 +1,13 @@
+
+
+from utils.utils.strings import compare_strings
+
+
+def start_app():
+    print('Starting application...')
+    print(compare_strings('A', 'B'))
+    print('Done')
+
+
+if __name__ == '__main__':
+    start_app()
